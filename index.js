e9c7ee30049c42465b34a9f845814f2d5d2aa1e7
YurijCroz <valetkrest@gmail.com>
2022-07-06 21:24:46 +0300

    adding content from js

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 
-const textContainer = document.querySelector('.text-container');
-const imagesContainer = document.querySelector('.images-container');
-const imageBlock = document.querySelector('.image-block');
+const mainContainer = document.querySelector('.main-container');
+
+const arrowUp = document.createElement('div');
+arrowUp.classList.add('arrow','arrow-up');
+arrowUp.innerHTML = '<i class="fa-solid fa-arrow-up-long"></i>';
+const arrowDown = document.createElement('div');
+arrowDown.classList.add('arrow','arrow-down');
+arrowDown.innerHTML = '<i class="fa-solid fa-arrow-down-long"></i>';
+
+const textContainer = document.createElement('div');
+textContainer.classList.add('text-container');
+const imagesContainer = document.createElement('div');
+imagesContainer.classList.add('images-container');
+
+textContainer.innerHTML = '<div class="text-block"><h3>Lorem, ipsum.1</h3></div><div class="text-block"><h3>Lorem, ipsum dolor.2</h3></div><div class="text-block"><h3>Lorem, ipsum.3</h3></div><div class="text-block"><h3>Lorem ipsum dolor sit.4</h3></div>';
+imagesContainer.innerHTML = '<img src="./images/1.jpeg" alt="img"><img src="./images/2.jpeg" alt="img"><img src="./images/3.jpeg" alt="img"><img src="./images/4.jpeg" alt="img">';
+
+mainContainer.append(arrowUp, arrowDown, textContainer, imagesContainer);
+
+const imageBlock = document.querySelector('.images-container img');
 const heightBlock = imageBlock.clientHeight;
 const amount = imagesContainer.childElementCount;
-const arrowUp = document.querySelector('.arrow-up');
-const arrowDown = document.querySelector('.arrow-down');
 
 
 let count = 0;
-let max = heightBlock*(amount-1);
+const max = heightBlock*(amount-1);
 function upFun() {
     count++;
     if(count>(amount-1)) {count=0};
